1d0b5bb187dbb2bd26fcb286cec819adc66413ad
Sławomir Śledź <slawomir.sledz@gmail.com>
2020-02-08 23:58:29 +0100

    Initial commit

--- README.md
+++ README.md
@@ -13,7 +13,8 @@ docker-compose scale kafka=2
 ## Testing
 
 ```
-$KAFKA_HOME/bin/kafka-topics.sh --create --topic topic --partitions 4 --zookeeper $ZK --replication-factor 2
+$KAFKA_HOME/bin/kafka-topics.sh --create --topic topic --partitions 4 \
+  --zookeeper $ZK --replication-factor 2
 $KAFKA_HOME/bin/kafka-topics.sh --describe --topic topic --zookeeper $ZK
 $KAFKA_HOME/bin/kafka-console-producer.sh --topic=topic --broker-list $BRL
 ```
--- kafka-shell.sh
+++ kafka-shell.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-set -x
 brokers=$(docker ps --filter name=kafka_kafka --format "{{.Names}}" | sed s'/$/:9092/g' | tr '\n' ',' | sed 's/,$//')
-docker run --rm --network kafka_default -v /var/run/docker.sock:/var/run/docker.sock -e BRL=$brokers -e ZK=kafka_zookeeper_1:2181 -it wurstmeister/kafka /bin/bash
+docker run --rm --network kafka_default \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -e BRL=$brokers \
+  -e ZK=kafka_zookeeper_1:2181 \
+  -it \
+  wurstmeister/kafka:2.12-2.4.0 \
+  /bin/bash
