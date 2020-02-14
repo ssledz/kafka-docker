b60492976275b1b9a58d77d540865a18ec72788a
Sławomir Śledź <slawomir.sledz@cortb.pl>
2020-02-14 23:39:37 +0100

    adjusting kafka-shell.sh

--- kafka-shell.sh
+++ kafka-shell.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
-label="io.github.ssledz.service=kafka"
-brokers=$(docker ps --filter "label=$label" --format "{{.Names}}" | sed s'/$/:9092/g' | tr '\n' ',' | sed 's/,$//')
-network=$(docker ps --filter "label=$label" --format "{{.Networks}}")
-docker run --rm --network $network \
+kafka_label="io.github.ssledz.service=kafka"
+zookeeper_label="io.github.ssledz.service=zookeeper"
+kafka_name=$(docker ps --filter "label=$kafka_label" --format "{{.Names}}")
+zookeeper_name=$(docker ps --filter "label=$zookeeper_label" --format "{{.Names}}")
+kafka_port=$(docker port $(echo "$kafka_name" | head -1) | sed  -r 's/(.*)\/.*/\1/g')
+zookeper_port=$(docker port "$zookeeper_name" | sed  -r 's/(.*)\/.*/\1/g')
+brokers=$(echo "$kafka_name" | sed s'/$/:'"$kafka_port"'/g' | tr '\n' ',' | sed 's/,$//')
+zookeeper="zookeeper:$zookeper_port"
+network=$(docker ps --filter "label=$kafka_label" --format "{{.Networks}}" | head -1)
+echo "brokers: $brokers"
+echo "zookeeper: $zookeeper"
+echo "network: $network"
+
+docker run --rm --network "$network" --link "$zookeeper_name:zookeeper" \
   -v /var/run/docker.sock:/var/run/docker.sock \
-  -e BRL=$brokers \
-  -e ZK=kafka_zookeeper_1:2181 \
+  -e BRL="$brokers" \
+  -e ZK="$zookeeper" \
   -it \
   wurstmeister/kafka:2.12-2.4.0 \
   /bin/bash
